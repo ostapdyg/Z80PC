9365f55d7ff98742f9fd6aa3486cb362c533d1f9
Centurion256 <vkuchynskyy@gmail.com>
2019-12-23 14:01:45 +0200

    Expanded ZPC_setup ArduinoInit()

--- FunctionLibrary/ZPC_setup.c
+++ FunctionLibrary/ZPC_setup.c
@@ -7,8 +7,22 @@ void ZPC_ArduinoInit()
     pinMode(WR_, OUTPUT);
     pinMode(MREQ_, OUTPUT);
     pinMode(RD_, OUTPUT);
+    pinMode(RESET_, OUTPUT);
+    pinMode(WAIT_RES_, OUTPUT);
 
     digitalWrite(WR_, HIGH);
     digitalWrite(MREQ_, HIGH);
     digitalWrite(RD_, HIGH);
+    digitalWrite(RESET_, LOW);
+    digitalWrite(WAIT_RES_, LOW);
+
+    pinMode(INT_, INPUT_PULLUP);
+    pinMode(INT_, OUTPUT);
+    digitalWrite(INT_, HIGH);
+
+    pinMode(BUSACK_, INPUT);
+    pinMode(WAIT_, INPUT);
+    pinMode(BUSREQ_, INPUT_PULLUP);
+    pinMode(BUSREQ_, OUTPUT);
+    digitalWrite(BUSREQ_, HIGH);
 }
