996e0cba1cdeb17b3d62296669c08c8f80160741
ostapdyg <dyhdalovych@ucu.edu.ua>
2020-05-14 18:15:37 +0300

    Visualisation upd

--- ShowMemory/src/main.cpp
+++ ShowMemory/src/main.cpp
@@ -1,8 +1,9 @@
 #include <PCD8544.h>
+#include "ZPC_funcs.h"
 
 int randint(int n) { return rand() % (n); }
-static const byte colon[] = { B01100011, B01100011, B00000000};
-static const byte space[] = { B00000000, B00000000, B00000000};
+static const byte colon[] = {B01100011, B01100011, B00000000};
+static const byte space[] = {B00000000, B00000000, B00000000};
 
 struct LCD_State
 {
@@ -18,8 +19,7 @@ struct LCD_State
   uint8_t cur_reg2 = 0;
   PCD8544 lcd;
 
-// static const uint8_t colon[4]{ B00010000, B00110100, B00110000, B00110100, B00010000 };
-
+  // static const uint8_t colon[4]{ B00010000, B00110100, B00110000, B00110100, B00010000 };
 
   LCD_State(PCD8544 lcd) : lcd{lcd}
   {
@@ -37,24 +37,28 @@ struct LCD_State
     lcd.print(buf);
   }
 
-  void print_ram_row(uint8_t i){
-    if(i>4)return;
+  void print_ram_row(uint8_t i)
+  {
+    if (i > 4)
+      return;
     char buf[8];
-    lcd.setCursor(0, i+1);
-    sprintf(buf, "%04X", ram_addr+i*4);
+    lcd.setCursor(0, i + 1);
+    sprintf(buf, "%04X", ram_addr + i * 4);
     lcd.print(buf);
     // cld.write(" ");
     lcd.drawBitmap(colon, 3, 1);
-    for(int offset=0; offset<4; offset++){
-      sprintf(buf, "%02X", ram_data[i*4+offset]);
+    for (int offset = 0; offset < 4; offset++)
+    {
+      sprintf(buf, "%02X", ram_data[i * 4 + offset]);
       lcd.drawBitmap(space, 2, 1);
       lcd.print(buf);
-
     }
   }
 
-  void display_ram(){
-    for(int i=0; i<5; i++){
+  void display_ram()
+  {
+    for (int i = 0; i < 5; i++)
+    {
       print_ram_row(i);
     }
   }
@@ -67,28 +71,36 @@ struct LCD_State
     print_reg(cur_reg2);
   }
 
-  void begin(){
+  void begin()
+  {
     lcd.begin(84, 48);
   }
 
-  void next_reg(){
-    cur_reg2 = (cur_reg2+1)%7;
+  void next_reg()
+  {
+    cur_reg2 = (cur_reg2 + 1) % 7;
   }
 
+  void update_ram()
+  {
+    digitalWrite(BUSREQ_, LOW);
+    for (uint8_t offset = 0; offset < 24; offset++)
+    {
+      ram_data[offset] = ZPC_MemRead(ram_addr + offset);
+    }
+    digitalWrite(BUSREQ_, HIGH);
+  }
 
-  void update_ram(){
+  void update_regs()
+  {
     // TODO: implement...
   }
 
-  void update_regs(){
-    // TODO: implement...
-  }
-
-  void set_ram_addr(uint16_t addr){
+  void set_ram_addr(uint16_t addr)
+  {
     ram_addr = addr;
     update_ram();
   }
-
 };
 
 static PCD8544 lcd(23, 25, 27, 31, 29);
@@ -99,13 +111,10 @@ void setup()
   state.begin();
   Serial.begin(9600);
 
-  state.next_register();
   state.display_header();
   state.display_ram();
-  // lcd.print('a');
 }
 
 void loop()
 {
-
 }
