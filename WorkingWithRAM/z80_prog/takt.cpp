78ee239c1c526e794ce4c9f1048849a6cc2651f1
Danylo Sahaidak <danylos2001@gmail.com>
2020-06-14 15:31:08 +0300

    Add timing module

---
+++ WorkingWithRAM/z80_prog/takt.cpp
@@ -0,0 +1,36 @@
+#DEFINE interruptPin 19
+#DEFINE timingPin 5
+#DEFINE statePin0 6
+#DEFINE statePin1 7
+
+// 0 - clock
+// 1 - while
+// 2 - manual
+
+uint_8 timingMode = 0x01b;
+
+void pre () {
+    pinMode(timingPin, INPUT_PULLUP);
+    pinMode(statePin0, INPUT_PULLUP);
+    pinMode(statePin1, INPUT_PULLUP);
+    pinMode(interruptPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+}
+
+void change_timing () {
+
+    int newTimingMode = digitalRead(statePin0) + 2*digitalRead(statePin1);
+    if (newTimingMode != 3) timingMode = newTimingMode;
+
+}
+
+void loop () {
+    
+
+    if (timingMode == 2) {
+        while (!digitalRead(timingPin)) {
+        }
+    }
+    
+    
+}
