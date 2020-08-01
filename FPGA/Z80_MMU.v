f44d75f8a7e53038ffc9eee6cd1bc68320c18311
umbrelluck <yarko.umbrelluck@gmail.com>
2020-08-01 20:15:59 +0300

    MMU from master
    # Conflicts:
    #	FPGA/Z80_MMU.v

--- FPGA/Z80_MMU.v
+++ FPGA/Z80_MMU.v
@@ -1,41 +1,66 @@
-// addr_get: 0xffbb -> ff -> a[ff] -> XX -> 0xXXbb
-//data_forward: addr_get -> data, addr
-// 0x01XX -> 01 -> a[XX] = data
-
-//module addr_get: addr -> addr
-//module addr_set: addr, data -> null
-//module Z80_MMU: addr, data -> addr, data/null
-module Z80_MMU  #(parameter SIZE = 256)(
-					input wire[15:0] virtual_addr,
-					input wire[7:0] data,
-					output reg[15:0] physical_addr
+module Z80_MMU  #(parameter FLAGS = 4,
+				  parameter PA = 12,
+				  parameter SIZE = 256)(
+					input wire nMREQ,
+					input wire nRD,
+					input wire nWR,
+					inout wire[7:0] ram_data,
+					inout wire[7:0] cpu_data,
+					input wire[15:0] cpu_addr,
+					output reg[PA+8-1:0] ram_addr
 				);
 
-	reg[7:0] page_table[SIZE-1:0];
+	reg [FLAGS+PA-1:0] page_table[SIZE-1:0];
 	wire [7:0] page_name;
 	wire [7:0] offset;
-	
-	// setting initial page_table adresses to 0
+	wire [19:0] physical_addr;
+	// set initial page_table adresses to 16'hff - i (reverse descending order).
 	integer i;
 	initial 
 	begin
+        ram_addr = 20'hz;
 		for (i=0; i<SIZE; i=i+1) 
 		begin
 			page_table[i] = 0;
 		end
 	end
 
-	// if virtual_addr is x00XX => setting XX page_table entry to data
-	assign page_name = virtual_addr[15:8];
-	assign offset = virtual_addr[7:0];
-	always @(virtual_addr)
-	begin
-		if (page_name == 0) 
-			page_table[offset][7:0] = data[7:0];
-		else 
-			physical_addr[15:0] = {page_table[page_name], virtual_addr[7:0]};
-	end
+	// ram_address[0] == 0 -> low PA
+	// ram_address[0] == 1 -> high PA
+	assign page_name = cpu_addr[15:8];
+	assign offset = cpu_addr[7:0];
+	assign physical_addr[19:0] = {page_table[page_name][PA-1:0], offset};
 
+    assign cpu_data = (~nRD) ? ram_data : 8'bz; // if we are reading, then take data from ram, else set cpu_data to z-state(release the bus)
+    assign ram_data = (~nWR & (physical_addr[19:8] ^ 8'hfe >= 2)) ? cpu_data : 8'bz; //if the CPU is writing and it's not changing the page table, forward the data from it's inner bus to the outer bus. Otherwise, let go of the CPU data bus.
+
+
+
+	always @(nMREQ or nWR or nRD)
+		begin
+		if(~nMREQ)
+		begin
+        	if (physical_addr[19:8] ^ 8'hfe < 2 && ~nWR)
+				begin
+					if(cpu_addr[0] == 0)
+						begin
+
+						$strobe("Page table write: A-%x, D-%x, byte - %b",physical_addr[8:1], cpu_data, physical_addr[0]);
+						page_table[physical_addr[8:1]][7:0] = cpu_data[7:0]; //Change the page table entry
+						end
+					else
+						begin
+						page_table[physical_addr[8:1]][FLAGS+PA-1:8] = cpu_data[7:0]; //Change the page table entry
+						end	
+				end
+			else
+				begin
+					ram_addr[PA+8-1:0] = physical_addr; //Set the physical address
+				end
+		end
+		else
+			ram_addr[PA+8-1:0] = 20'hz; // $strobe("nMREQ is inactive");
+		end
 	
 endmodule
 
